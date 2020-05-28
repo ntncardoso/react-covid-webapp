879e377dec06ca967874ad668cbe65b9f296233a
ntncardoso <ntncardoso9@gmail.com>
2020-05-28 19:28:00 +0100

    branch dev first commit

--- covid-webapp/src/App.js
+++ covid-webapp/src/App.js
@@ -1,26 +1,77 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Container, Col, Nav, Row } from 'react-bootstrap'
+import { Route, Switch } from 'react-router-dom'
+import { LinkContainer } from 'react-router-bootstrap'
+//import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const URI = 'https://disease.sh/v2/all'
+
+class App extends React.Component {
+
+  state = {
+    name: 'cardoso',
+    data: {},
+  }
+
+  getData = () => {
+    fetch(URI)
+      .then(response => response.json())
+      .then(num => {
+        this.setState({ data: num})
+      })
+  }
+
+  componentDidMount() {
+    this.getData()
+  }
+
+  render() {
+    let Cases = () => <h1>{this.state.data.cases}</h1>
+    let Deaths = () => <h1>{this.state.data.deaths}</h1>
+    let Recovered = () => <h1>{this.state.data.recovered}</h1>
+
+    return(
+      <Container fluid>
+        <Row>
+          <Col sm={4}>
+            <Nav fill variant="tabs" defaultActiveKey="/cases">
+              <Nav.Item>
+                <LinkContainer to="/cases">
+                  <Nav.Link>Cases</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+              <Nav.Item>
+                <LinkContainer to="/deaths">
+                  <Nav.Link>Deaths</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+              <Nav.Item>
+                <LinkContainer to="/recovered">
+                  <Nav.Link>Recovered</Nav.Link>
+                </LinkContainer>
+              </Nav.Item>
+            </Nav>
+          </Col>
+          <Col sm={8}>
+            <Switch>
+              <Route path="/cases">
+                <Cases />
+              </Route>
+              <Route path="/deaths">
+                <Deaths />
+              </Route>
+              <Route path="/recovered">
+                <Recovered />
+              </Route>
+            </Switch>
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+
 }
+
+
 
 export default App;
