125ff7e8872f2c698c76436f1274a92624e7b588
ntncardoso <ntncardoso9@gmail.com>
2020-06-04 18:52:27 +0100

    refactor components

--- covid-webapp/src/App.jsx
+++ covid-webapp/src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import {Container, Col, Nav, Row } from 'react-bootstrap'
+import {Container} from 'react-bootstrap'
 import {  Route, Switch, BrowserRouter} from 'react-router-dom'
-import { LinkContainer } from 'react-router-bootstrap'
 //import './App.css';
-import CasesList from './Cases.jsx'
-import DeathsList from './Deaths'
-import RecoveredList from './Recovers'
+import NavRender from './Nav'
 
 const URI = 'https://disease.sh/v2/all'
 
@@ -28,9 +25,6 @@ class App extends React.Component {
   }
 
   render() {
-    let Cases = () => <h1>{this.state.data.cases}</h1>
-    let Deaths = () => <h1>{this.state.data.deaths}</h1>
-    let Recovered = () => <h1>{this.state.data.recovered}</h1>
 
     return(
       <BrowserRouter>
@@ -38,34 +32,15 @@ class App extends React.Component {
         <div className="row h-100 p-2">
           <div className="col-md-3 h-100 p-2">
             <div className="h-5">
-              <Nav className="nav nav-tabs h-100 border-0 d-flex bg-light" defaultActiveKey="/cases">
-                <Nav.Item className="nav-item h-100 flex-fill m-0">
-                  <LinkContainer to="/cases">
-                    <Nav.Link>Cases</Nav.Link>
-                  </LinkContainer>
-                </Nav.Item>
-                <Nav.Item className="nav-item h-100 flex-fill m-0" >
-                  <LinkContainer to="/deaths">
-                    <Nav.Link>Deaths</Nav.Link>
-                  </LinkContainer>
-                </Nav.Item>
-                <Nav.Item className="nav-item h-100 flex-fill m-0">
-                  <LinkContainer to="/recovered">
-                    <Nav.Link>Recovered</Nav.Link>
-                  </LinkContainer>
-                </Nav.Item>
-              </Nav>
+             <NavRender />
             </div>
             <div className="h-95">
               <Switch>
-                <Route path="/cases">
-                  <CasesList />
+                <Route path="/Cases">
                 </Route>
-                <Route path="/deaths">
-                  <DeathsList />
+                <Route path="/Deaths">
                 </Route>
-                <Route path="/recovered">
-                  <RecoveredList  />
+                <Route path="/Recovered">
                 </Route>
               </Switch>
             </div>
---
+++ covid-webapp/src/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Nav} from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+
+
+export default function NavRender(props) {
+    const types = ['Cases', 'Deaths', 'Recovered']
+
+    return (
+        <Nav className="nav nav-tabs h-100 border-0 d-flex bg-light" defaultActiveKey="/Cases">
+            {types.map((type) => 
+            
+            <Nav.Item className="nav-item h-100 flex-fill m-0" key={type} 
+                    onClick={() => props.onSelectLanguage(type)}>
+                <LinkContainer to={'/' + type}>
+                    <Nav.Link>{type}</Nav.Link>
+                </LinkContainer>
+            </Nav.Item>
+            )}
+        </Nav>
+    )
+}
+
